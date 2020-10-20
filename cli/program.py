1b8ed7151e056d49f96d8280e7e277c7f22b394d
Sebastian Mellen <s@slm.space>
2020-10-20 00:02:34 -0700

    [feat] Add python CLI stub

---
+++ cli/program.py
@@ -0,0 +1,25 @@
+import click
+
+
+@click.command()
+@click.option('--task', help='Add a new task to a routine.')
+@click.option('--routine', prompt='Create a new routine within a system. Use the format "hello"',
+              help='Create a new routine within a system. Use the format "ROUTINE_NAME/SYSTEM". If you want to change the name of a routine')
+@click.option('--system', prompt='Create a new system.',
+              help='The person to greet.')
+@click.option('--goal', prompt='Add a goal to a system.', help='Add a goal to a system')
+def hello(count, name):
+    """GettingBetter is a systems-based framework for improving your life.
+
+    The three basic functions provided by this CLI allow you to: 
+
+    1. Add a task to a routine\n
+    2. create a new routine, or\n 
+    3. create a new system."""
+
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
+
+if __name__ == '__main__':
+    hello()
